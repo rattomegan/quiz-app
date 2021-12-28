e83ab272001952d54dffdcedd56212600d7d9888
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2021-12-28 13:31:40 -0800

    Create progress bar

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("question-counter");
+const progressText = document.getElementById("progress-text");
 const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progress-bar-full");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -55,7 +56,9 @@ getNewQuestion = () => {
     return window.location.assign("/end.html")
   }
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  // Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
