df7b9865a3bb3025751dcf2c7facc8c2b2feb4c1
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2021-12-28 14:22:07 -0800

    Save high scores in local storage

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById('save-score-btn');
 const finalScore = document.getElementById('final-score');
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+// if local storage is empty, initialize an empty array so we are not receiveing a null value for highScores
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -11,7 +15,20 @@ username.addEventListener("keyup", () => {
 })
 
 saveHighScore = (e) => {
-  console.log('clicked the save button');
   // prevents form from automatically taking us to a new page
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+
+  // sort scores from highest to lowest
+  highScores.sort( (a, b) => b.score - a.score);
+
+  highScores.splice(5);
+  // saves the high scores even after a refresh
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 }
